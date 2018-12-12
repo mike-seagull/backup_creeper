fcc5ef1d6d5ffadbabb2f1f78310f007daee03c7
Michael Hollister <hollister.michael@gmail.com>
2018-12-12 15:46:39 -0600

    trying to get deployment working

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+WORKING_DIR=$1
 
-mv id_rsa ~/.ssh/
+ls
+mv ${WORKING_DIR}/id_rsa ~/.ssh/
 sudo chmod 600 ~/.ssh/id_rsa
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $(pwd)/bin/backup_sweeper ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/backup_sweeper
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${REMOTE_USER}@${REMOTE_SERVER} /bin/chmod +x /usr/local/bin/backup_sweeper
