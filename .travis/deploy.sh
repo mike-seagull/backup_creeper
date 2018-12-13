fb3b2a3280776a29af0e2f0bd35d5a97d3b9ce20
Michael Hollister <hollister.michael@gmail.com>
2018-12-13 01:17:03 -0600

    adding ssh to known_hosts

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $(pwd)/bin/backup_sweeper ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/backup_sweeper
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${REMOTE_USER}@${REMOTE_SERVER} /bin/chmod +x /usr/local/bin/backup_sweeper
+scp $(pwd)/bin/backup_sweeper ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/backup_sweeper
+ssh ${REMOTE_USER}@${REMOTE_SERVER} /bin/chmod +x /usr/local/bin/backup_sweeper
