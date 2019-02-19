7d811d6cbd009eb3fe0e5f39a534994ec73b1ae8
Michael Hollister <hollister.michael@gmail.com>
2019-02-18 23:16:57 -0600

    moved deployment files to dropbox

--- .travis/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-scp  -o LogLevel=quiet $(pwd)/bin/backup_creeper ${REMOTE_USER}@${REMOTE_SERVER}:/usr/local/bin/backup_creeper
-ssh  -o LogLevel=quiet ${REMOTE_USER}@${REMOTE_SERVER} /bin/chmod +x /usr/local/bin/backup_creeper
